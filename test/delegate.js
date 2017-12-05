3d01b201c6d19cf818c2db8629682a91a6163e8c
Federico Brigante <github@bfred.it>
2017-12-06 02:12:12 +0800

    Add tests

--- test/delegate.js
+++ test/delegate.js
@@ -38,4 +38,33 @@ describe('delegate', function() {
         delegation.destroy();
         assert.ok(global.spy.calledOnce);
     });
+
+    it('should use `document` if the element is unspecified', function(done) {
+        delegate('a', 'click', function() {
+            done();
+        });
+
+        simulant.fire(global.anchor, simulant('click'));
+    });
+
+    it('should add event listeners to all the elements in a base selector', function() {
+        var spy = sinon.spy();
+        delegate('li', 'a', 'click', spy);
+
+        var anchors = document.querySelectorAll('a')
+        simulant.fire(anchors[0], simulant('click'));
+        simulant.fire(anchors[1], simulant('click'));
+        assert.ok(spy.calledTwice);
+    });
+
+    it('should add event listeners to all the elements in a base array', function() {
+        var spy = sinon.spy();
+        var items = document.querySelectorAll('a')
+        delegate(items, 'a', 'click', spy);
+
+        var anchors = document.querySelectorAll('a')
+        simulant.fire(anchors[0], simulant('click'));
+        simulant.fire(anchors[1], simulant('click'));
+        assert.ok(spy.calledTwice);
+    });
 });
